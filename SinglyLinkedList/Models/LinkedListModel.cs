1e4320331c5eba962e6febc29d74bae3e0ab3b69
Alexandrina <sunnydimitrova19@gmail.com>
2022-02-17 12:22:31 +0200

    Initial commit

---
+++ SinglyLinkedList/Models/LinkedListModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList.Models
+{
+    public class LinkedListModel
+    {
+        private Node head;
+
+        private class Node
+        {
+            internal int data;
+            internal Node next;
+            internal Node(int data)
+            {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public void Add(int element)
+        {
+            var node = new Node(element);
+            node.next = this.head;
+            this.head = node;
+        }
+
+        public string ReturnMthFromLast(int m)
+        {
+            var pointer = this.head;
+
+            if (pointer != null && m >= 0)
+            {
+                if (m == 0)
+                {
+                    return pointer.data.ToString();
+                }
+                for (int i = 0; i < m; i++)
+                {
+                    if (pointer.next != null)
+                    {
+                        pointer = pointer.next;
+                    }
+                    else
+                    {
+                        return "Invalid M.";
+                    }
+                }
+
+                return pointer.data.ToString();
+            }
+            else
+            {
+                return "Invalid data.";
+            }
+        }
+
+        public bool IsEmpty() => head == null;
+       
+    }
+}
---
+++ SinglyLinkedList/ViewModels/ShellViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Caliburn.Micro;
+using SinglyLinkedList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private LinkedListModel linkedList = new LinkedListModel();
+        private BindableCollection<Item> items = new BindableCollection<Item>();
+        private int number;
+        private int m;
+
+        public int M
+        {
+            get { return m; }
+            set { 
+                m = value;
+                NotifyOfPropertyChange(() => M);
+            }
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set { 
+                number = value;
+                NotifyOfPropertyChange(() => Number);
+            }
+        }
+
+        private string answer;
+
+        public string Answer
+        {
+            get { return answer; }
+            set
+            { 
+                answer = value;
+                NotifyOfPropertyChange(() => Answer);
+            }
+        }
+
+        public BindableCollection<Item> Items
+        {
+            get { return items; }
+            set 
+            { 
+                items = value;
+                NotifyOfPropertyChange(() => Items);
+            }
+        }
+
+
+        public LinkedListModel LinkedList
+        {
+            get { return linkedList; }
+            set 
+            { 
+                linkedList = value;
+                NotifyOfPropertyChange(() => LinkedList);
+            }
+        }
+
+        public void AddNumberToLinkedList(int number)
+        {
+            if (LinkedList.IsEmpty())
+            {
+                Items = new BindableCollection<Item>();
+            }
+            LinkedList.Add(number);
+            Items.Add(new Item {CurrentItem = number });
+            
+            Number = 0;
+            Answer = "";
+        }
+
+        public bool CanAddNumberToLinkedList(int number) => number != 0;
+
+        public void MthNumber(int m)
+        {
+            Answer = LinkedList.ReturnMthFromLast(m);
+            M = 0;
+            LinkedList = new LinkedListModel();
+        }
+
+        public bool CanMthNumber(int m) => Items.Count > 0;
+    }
+}
